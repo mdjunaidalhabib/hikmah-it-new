a08839686a32bc607e23d14cf68f493fe3aaac86
mdjunaidalhabib <mdjunaidalhabib20@gmail.com>
2026-05-21 12:57:06 +0600

    Updated code

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ export default function Logo() {
     <a className="flex shrink-0 items-center" href="#home" aria-label="Hikmah IT home">
       <img
         className="h-10 w-[160px] object-contain object-left drop-shadow-lg sm:h-11 sm:w-[175px] lg:h-12 lg:w-[185px]"
-        src="/hikmah-it-navbar-logo.png"
+        src="/hikmah-it-navbar-logo.svg"
         alt="Hikmah IT"
       />
     </a>
